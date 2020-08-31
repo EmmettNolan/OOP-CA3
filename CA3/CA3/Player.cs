04b40c23c836f4e97f8ba5da6e88373f5d57e414
S00188098 <S00188098@mail.itsligo.ie>
2020-08-31 01:23:17 +0100

    Player Class Created

---
+++ CA3/CA3/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA3
+{
+    class Player
+    {
+        public enum Position { Goalkeeper, Defender, Midfielder, Forward }
+
+        //Properties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Position PerferredPosition { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public int age
+        {
+            get
+            {
+                Age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateOfBirth.DayOfYear > DateTime.Now.DayOfYear)
+                    Age--;
+                return Age;
+            }
+        }
+    }
+}
