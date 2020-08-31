d8ac07892848d739dba983d732b0cace318fe0a3
S00188098 <S00188098@mail.itsligo.ie>
2020-08-31 15:52:31 +0100

    Updates

--- CA3/CA3/MainWindow.xaml.cs
+++ CA3/CA3/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace CA3
         List<Player> allPlayers = new List<Player>();
         List<Player> selectedPlayers = new List<Player>();
 
+        public string Surname { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,8 +33,81 @@ namespace CA3
 
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
+            //Methods
+
+            //Random Player Creation
+            void RandName()
+            {
+                string[] firstNames = new string[]
+                {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+
+                string[] lastNames = new string[]
+                    {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+                    };
+
+                Random rnd = new Random();
+                int index = rnd.Next(firstNames.Length) + rnd.Next(lastNames.Length);
+            }
+
+            //Create List
+            void PlayerList()
+            {
+                RandName();
+                Player p1 = new Player(Name, Surname, Player.Position.Goalkeeper);
+                Player p2 = new Player(Name, Surname, Player.Position.Goalkeeper);
+                Player p3 = new Player(Name, Surname, Player.Position.Defender);
+                Player p4 = new Player(Name, Surname, Player.Position.Defender);
+                Player p5 = new Player(Name, Surname, Player.Position.Defender);
+                Player p6 = new Player(Name, Surname, Player.Position.Defender);
+                Player p7 = new Player(Name, Surname, Player.Position.Defender);
+                Player p8 = new Player(Name, Surname, Player.Position.Defender);
+                Player p9 = new Player(Name, Surname, Player.Position.Midfielder);
+                Player p10 = new Player(Name, Surname, Player.Position.Midfielder);
+                Player p11 = new Player(Name, Surname, Player.Position.Midfielder);
+                Player p12 = new Player(Name, Surname, Player.Position.Midfielder);
+                Player p13 = new Player(Name, Surname, Player.Position.Midfielder);
+                Player p14 = new Player(Name, Surname, Player.Position.Midfielder);
+                Player p15 = new Player(Name, Surname, Player.Position.Forward);
+                Player p16 = new Player(Name, Surname, Player.Position.Forward);
+                Player p17 = new Player(Name, Surname, Player.Position.Forward);
+                Player p18 = new Player(Name, Surname, Player.Position.Forward);
+                Player p19 = new Player("Emmett", "Nolan", Player.Position.Defender);
+
+                List<Player> allPlayers = new List<Player>();
+
+                allPlayers.Add(p1);
+                allPlayers.Add(p2);
+                allPlayers.Add(p3);
+                allPlayers.Add(p4);
+                allPlayers.Add(p5);
+                allPlayers.Add(p6);
+                allPlayers.Add(p7);
+                allPlayers.Add(p8);
+                allPlayers.Add(p9);
+                allPlayers.Add(p10);
+                allPlayers.Add(p11);
+                allPlayers.Add(p12);
+                allPlayers.Add(p13);
+                allPlayers.Add(p14);
+                allPlayers.Add(p15);
+                allPlayers.Add(p16);
+                allPlayers.Add(p17);
+                allPlayers.Add(p18);
+                allPlayers.Add(p19);
+            }
+
+
+            allPlayersLB.ItemsSource = allPlayers;
+
             //Sorting by first name then second name.
-            allPlayersLB.ItemsSource = allPlayers.OrderBy(s => s.Name).ThenBy(s => s.Surname);
+            //allPlayersLB.ItemsSource = allPlayers.OrderBy(s => s.Name).ThenBy(s => s.Surname);
 
             // Setting up the contents of the ComboBox
             formationsCbx.ItemsSource = new string[] { "4-4-2", "4-3-3", "4-5-1" };
--- CA3/CA3/Player.cs
+++ CA3/CA3/Player.cs
@@ -35,72 +35,72 @@ namespace CA3
             PerferredPosition = perferredPosition;
         }
 
-        //Methods
+        ////Methods
 
-        //Random Player Creation
-        private void RandName()
-        {
-            string[] firstNames = new string[]
-            {
-                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
-                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
-                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+        ////Random Player Creation
+        //private void RandName()
+        //{
+        //    string[] firstNames = new string[]
+        //    {
+        //        "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+        //        "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+        //        "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
 
 
-            string[] lastNames = new string[]
-                {
-                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
-                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
-                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
-                };
+        //    string[] lastNames = new string[]
+        //        {
+        //        "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+        //        "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+        //        "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+        //        };
 
-            Random rnd = new Random();
-            int index = rnd.Next(firstNames.Length) + rnd.Next(lastNames.Length);
-        }
+        //    Random rnd = new Random();
+        //    int index = rnd.Next(firstNames.Length) + rnd.Next(lastNames.Length);
+        //}
 
-        //Create List
-        private void PlayerList()
-        {
-            Player p1 = new Player(Name, Surname, Player.Position.Goalkeeper);
-            Player p2 = new Player(Name, Surname, Player.Position.Goalkeeper);
-            Player p3 = new Player(Name, Surname, Player.Position.Defender);
-            Player p4 = new Player(Name, Surname, Player.Position.Defender);
-            Player p5 = new Player(Name, Surname, Player.Position.Defender);
-            Player p6 = new Player(Name, Surname, Player.Position.Defender);
-            Player p7 = new Player(Name, Surname, Player.Position.Defender);
-            Player p8 = new Player(Name, Surname, Player.Position.Defender);
-            Player p9 = new Player(Name, Surname, Player.Position.Midfielder);
-            Player p10 = new Player(Name, Surname, Player.Position.Midfielder);
-            Player p11 = new Player(Name, Surname, Player.Position.Midfielder);
-            Player p12 = new Player(Name, Surname, Player.Position.Midfielder);
-            Player p13 = new Player(Name, Surname, Player.Position.Midfielder);
-            Player p14 = new Player(Name, Surname, Player.Position.Midfielder);
-            Player p15 = new Player(Name, Surname, Player.Position.Forward);
-            Player p16 = new Player(Name, Surname, Player.Position.Forward);
-            Player p17 = new Player(Name, Surname, Player.Position.Forward);
-            Player p18 = new Player(Name, Surname, Player.Position.Forward);
+        ////Create List
+        //private void PlayerList()
+        //{
+        //    Player p1 = new Player(Name, Surname, Player.Position.Goalkeeper);
+        //    Player p2 = new Player(Name, Surname, Player.Position.Goalkeeper);
+        //    Player p3 = new Player(Name, Surname, Player.Position.Defender);
+        //    Player p4 = new Player(Name, Surname, Player.Position.Defender);
+        //    Player p5 = new Player(Name, Surname, Player.Position.Defender);
+        //    Player p6 = new Player(Name, Surname, Player.Position.Defender);
+        //    Player p7 = new Player(Name, Surname, Player.Position.Defender);
+        //    Player p8 = new Player(Name, Surname, Player.Position.Defender);
+        //    Player p9 = new Player(Name, Surname, Player.Position.Midfielder);
+        //    Player p10 = new Player(Name, Surname, Player.Position.Midfielder);
+        //    Player p11 = new Player(Name, Surname, Player.Position.Midfielder);
+        //    Player p12 = new Player(Name, Surname, Player.Position.Midfielder);
+        //    Player p13 = new Player(Name, Surname, Player.Position.Midfielder);
+        //    Player p14 = new Player(Name, Surname, Player.Position.Midfielder);
+        //    Player p15 = new Player(Name, Surname, Player.Position.Forward);
+        //    Player p16 = new Player(Name, Surname, Player.Position.Forward);
+        //    Player p17 = new Player(Name, Surname, Player.Position.Forward);
+        //    Player p18 = new Player(Name, Surname, Player.Position.Forward);
 
-            List<Player> allPlayers = new List<Player>();
+        //    List<Player> allPlayers = new List<Player>();
 
-            allPlayers.Add(p1);
-            allPlayers.Add(p2);
-            allPlayers.Add(p3);
-            allPlayers.Add(p4);
-            allPlayers.Add(p5);
-            allPlayers.Add(p6);
-            allPlayers.Add(p7);
-            allPlayers.Add(p8);
-            allPlayers.Add(p9);
-            allPlayers.Add(p10);
-            allPlayers.Add(p11);
-            allPlayers.Add(p12);
-            allPlayers.Add(p13);
-            allPlayers.Add(p14);
-            allPlayers.Add(p15);
-            allPlayers.Add(p16);
-            allPlayers.Add(p17);
-            allPlayers.Add(p18);
-        }
+        //    allPlayers.Add(p1);
+        //    allPlayers.Add(p2);
+        //    allPlayers.Add(p3);
+        //    allPlayers.Add(p4);
+        //    allPlayers.Add(p5);
+        //    allPlayers.Add(p6);
+        //    allPlayers.Add(p7);
+        //    allPlayers.Add(p8);
+        //    allPlayers.Add(p9);
+        //    allPlayers.Add(p10);
+        //    allPlayers.Add(p11);
+        //    allPlayers.Add(p12);
+        //    allPlayers.Add(p13);
+        //    allPlayers.Add(p14);
+        //    allPlayers.Add(p15);
+        //    allPlayers.Add(p16);
+        //    allPlayers.Add(p17);
+        //    allPlayers.Add(p18);
+        //}
 
         //Order FirstName, LastName, DateOfBirth, Position
         public override string ToString()
